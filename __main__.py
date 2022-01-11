9ba4fb47eaca9666e809712aa33a01665fac1763
Chris Lynch <chris.lynch@datacom.co.nz>
2022-01-11 13:22:35 +1300

    add code to build backend image

--- __main__.py
+++ __main__.py
@@ -1,3 +1,14 @@
-"""A Python Pulumi program"""
-
+import os
 import pulumi
+import pulumi_docker as docker
+
+stack = pulumi.get_stack()
+
+# build backend image
+backend_image_name = 'backend'
+backend = docker.Image(
+  backend_image_name,
+  build=docker.DockerBuild(context=f'{os.getcwd()}/app/backend'),
+  image_name=f'{backend_image_name}:{stack}',
+  skip_push=True
+)
